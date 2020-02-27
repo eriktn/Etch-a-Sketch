f4c1694a9c0340a37b8d2c0ff4417fe44f159946
eriktn <deziili27@gmail.com>
2020-02-27 13:52:02 +0100

    applied hover effect to div elements

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const container = document.querySelector('#container');
 let divs = new Array();
+
+function changeColor(e) {
+    e.target.style.backgroundColor = "red";
+}
+
 for (let i=0;i<256;i++) {
     divs[i] = document.createElement('div');
     divs[i].style.cssText = "border: 1px solid black; height: 15px; padding: 0; margin: 0";
+    divs[i].addEventListener('mouseenter', changeColor);
     container.appendChild(divs[i]);
 }
